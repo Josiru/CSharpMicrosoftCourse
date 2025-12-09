9f0212abf746f204720baca02bb0fe7df3fed357
Siran Laborin <Siran.Laborin@burrana.aero>
2025-12-09 06:47:50 -0800

    Initial

---
+++ CSharp/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,11 @@
+﻿Random dice = new Random(); //Instance of the class Random named dice
+// Random dice = new() -> does the same as above on latest .net.
+
+
+//New request address on the memory to store new objects on the Class
+//Creates a new memory and stores the addres.
+//returns the adresss memory to be used on dice.
+
+//Random class 
+int roll = dice.Next(1,7);
+Console.WriteLine(roll);
